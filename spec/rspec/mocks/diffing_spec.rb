c0ffe42152e6f1a8b2b006307c39758ae3f66fc9
Sam Phippen <samphippen@googlemail.com>
2014-09-10 10:07:24 +0100

    Spec cleanup for diffing

--- spec/rspec/mocks/diffing_spec.rb
+++ spec/rspec/mocks/diffing_spec.rb
@@ -1,6 +1,10 @@
 require "spec_helper"
 
 RSpec.describe "Diffs printed when arguments don't match" do
+  before do
+    allow(RSpec::Mocks.configuration).to receive(:color?).and_return(false)
+  end
+
   it "does not print a diff when single arguments are mismatched" do
     d = double("double")
     expect(d).to receive(:foo).with("some string")
@@ -24,7 +28,6 @@ RSpec.describe "Diffs printed when arguments don't match" do
   let(:actual_hash) { {:bad => :hash} }
 
   it "prints a diff with hash args" do
-    allow(RSpec::Mocks.configuration).to receive(:color?).and_return(false)
     d = double("double")
     expect(d).to receive(:foo).with(expected_hash)
     expect {
@@ -34,7 +37,6 @@ RSpec.describe "Diffs printed when arguments don't match" do
   end
 
   it "prints a diff with an expected hash arg and a non-hash actual arg" do
-    allow(RSpec::Mocks.configuration).to receive(:color?).and_return(false)
     d = double("double")
     expect(d).to receive(:foo).with(expected_hash)
     expect {
@@ -44,7 +46,6 @@ RSpec.describe "Diffs printed when arguments don't match" do
   end
 
   it "prints a diff with array args" do
-    allow(RSpec::Mocks.configuration).to receive(:color?).and_return(false)
     d = double("double")
     expect(d).to receive(:foo).with([:a, :b, :c])
     expect {
@@ -54,17 +55,43 @@ RSpec.describe "Diffs printed when arguments don't match" do
   end
 
   it "does not use the object's description for a non-matcher object that implements #description" do
-    allow(RSpec::Mocks.configuration).to receive(:color?).and_return(false)
     d = double("double")
 
-    #object here because double.description changes the inspect value
-    collab = Object.new.tap {|o| allow(o).to receive(:description).and_return("This string") }
+    collab = double(:collab, :description => "This string")
     collab_inspect = collab.inspect
 
     expect(d).to receive(:foo).with(collab)
     expect {
       d.foo([])
     }.to raise_error(RSpec::Mocks::MockExpectationError,  "Double \"double\" received :foo with unexpected arguments\n  expected: (#{collab_inspect})\n       got: ([]) \n@@ -1,2 +1,2 @@\n-[#{collab_inspect}]\n+[[]]\n")
+    reset d
+  end
+
+  it "does use the object's description for a matcher object that implements #description" do
+    d = double("double")
+
+    collab = double(:collab, :description => "This string")
+    allow(RSpec::Support).to receive(:is_a_matcher?).with(collab).and_return(true)
+    collab_description = collab.description
+
+    expect(d).to receive(:foo).with(collab)
+    expect {
+      d.foo([:a, :b])
+    }.to raise_error(RSpec::Mocks::MockExpectationError,  "Double \"double\" received :foo with unexpected arguments\n  expected: (#{collab_description})\n       got: ([:a, :b]) \n@@ -1,2 +1,2 @@\n-[\"#{collab_description}\"]\n+[[:a, :b]]\n")
+    reset d
+  end
+
+  it "does not use the object's description for a matcher object that does not implement #description" do
+    d = double("double")
+
+    collab = double(:collab)
+    allow(RSpec::Support).to receive(:is_a_matcher?).with(collab).and_return(true)
+    collab_inspect = collab.inspect
+
+    expect(d).to receive(:foo).with(collab)
+    expect {
+      d.foo([:a, :b])
+    }.to raise_error(RSpec::Mocks::MockExpectationError,  "Double \"double\" received :foo with unexpected arguments\n  expected: (#{collab_inspect})\n       got: ([:a, :b]) \n@@ -1,2 +1,2 @@\n-[#{collab_inspect}]\n+[[:a, :b]]\n")
     reset d
   end
 end
